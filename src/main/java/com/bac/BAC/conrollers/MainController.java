cd7def680dba7fe79e88614d1e701e2387eddee6
Catea13 <58842293+Catea13@users.noreply.github.com>
2020-10-10 18:37:58 +0300

    Initial commit

---
+++ src/main/java/com/bac/BAC/conrollers/MainController.java
@@ -0,0 +1,20 @@
+package com.bac.BAC.conrollers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Main Page");
+        return "home";
+    }
+    }
+
+
+
+
+
